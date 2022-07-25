76c2a2b0e32b8c3e480b399cc7efab5145266d16
Olumide Bambe <loyalsfc@gmail.com>
2022-07-25 23:41:54 +0100

    Integrated cart button on hover

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Products(props){
+    const [isHovered, setIsHovered] = useState(false)
+
+
+    console.log(isHovered)
     return(
-        <div style={{display: "flex", flexDirection: "column", width:"200px",height:"300px"}}>
+        <div 
+            style={
+                {display: "flex", 
+                flexDirection: "column", 
+                width:"200px",
+                height:"300px", 
+                color:"black"}
+            }
+            onMouseEnter={()=> setIsHovered(true)}
+            onMouseLeave={()=> setIsHovered(false)}
+        >
             <img src={props.img} height={150} width={100} />
             <h4>{props.title}</h4>
-            <span>{props.price}</span>
+            <span>${props.price}</span>
+            {isHovered && <button>Add to Cart</button>}
         </div>
     )
 }
